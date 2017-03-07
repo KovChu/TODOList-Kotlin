5e072f0f808e5cb7ecc3f5880d2f57dc56327efe
kuanyi <kuanyi@brightbitapps.com>
2017-03-08 01:26:32 +0800

    initial setup

---
+++ README.md
@@ -0,0 +1,24 @@
+# Todolist-Kotlin
+
+This is a rewrite of Todolist(https://github.com/KovChu/Todolist) in Kotlin as a practice project
+
+A simple ToDoList application that contains a list of ToDos. This application is created
+in mind for user to easily create/delete items, and filter the completed tasks to see which tasks are not complete yet.
+
+Function Specs:
+An todo item is created when the user press the floating action button
+Click on the item will mark it as complete, while long-click will delete the item.
+the menu also contains filter, mark-all-as-complete, mark-all-as-incomplete, and remove all items functionality.
+
+3rd Party Libraries used:
+
+Ormlite
+(http://ormlite.com/sqlite_java_android_orm.shtml)
+SQLite library for easy data structure and actions
+
+RecyclerView
+(https://github.com/twotoasters/RecyclerViewLib)
+for supporting RecyclerView for 4.0 and above and build-in animations on item modification.
+
+This project is committed to GitHub, and can be view at
+https://github.com/KovChu/Todolist-Kotlin
---
+++ app/src/main/java/com/demo/kuanyi/todolist_kotlin/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.demo.kuanyi.todolist_kotlin
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).show()
+//        }
+        var i = 3
+        fab.setOnClickListener { i ->
+            Log.i("Click", "i = " + i)
+            toast("hi")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        if (id == R.id.action_settings) {
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, duration).show()
+    }
+
+}
